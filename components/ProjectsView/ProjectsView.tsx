550a852a2c846f0a2c780cec8dc47206d7419205
Shalev Raz Israel <shalev280804@gmail.com>
2022-10-02 22:35:56 +0300

    Created menus on projectview

--- components/ProjectsView/ProjectsView.tsx
+++ components/ProjectsView/ProjectsView.tsx
@@ -1,11 +1,14 @@
-import { FC, useRef, useEffect } from 'react';
+import { FC, useRef, useEffect, useState } from 'react';
 import { Project } from '../../utils/supabase/projects';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardHeader from '@mui/material/CardHeader';
-import CardActionArea from '@mui/material/CardActionArea';
+import IconButton from '@mui/material/IconButton';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 import styles from './ProjectsView.module.css';
 
 type ProjectsViewProps = {
@@ -15,6 +18,8 @@ type ProjectsViewProps = {
 
 const ProjectsView: FC<ProjectsViewProps> = ({ projects, newProject }) => {
 	const projectsListRef = useRef<HTMLDivElement>(null);
+	const [openMenuIndex, setOpenMenuIndex] = useState<number>(-1);
+	const [anchor, setAnchor] = useState<HTMLButtonElement | null>(null);
 
 	useEffect(() => {
 		if (projectsListRef.current && newProject !== -1) {
@@ -22,19 +27,40 @@ const ProjectsView: FC<ProjectsViewProps> = ({ projects, newProject }) => {
 		}
 	}, [projectsListRef, newProject]);
 
+	function closeMenu() {
+		setOpenMenuIndex(-1);
+	}
+
+	function openMenu(index: number) {
+		return (e: React.MouseEvent<HTMLButtonElement>) => {
+			setAnchor(e.currentTarget);
+			setOpenMenuIndex(index);
+		};
+	}
+
 	return (
 		<Grid container spacing={4} ref={projectsListRef} sx={{ mb: 4 }}>
 			{projects.map((project, index) => (
 				<Grid item xs={12} sm={6} md={4} key={project.id}>
 					<Card variant="outlined" className={index === newProject ? styles.projectViewNew : ''}>
-						<CardActionArea>
-							<CardHeader title={project.name} subheader={new Date(project.created_at).toLocaleString()} />
-							<CardContent>
-								<Typography variant="body2" className={styles.projectViewCutText}>
-									{project.description}
-								</Typography>
-							</CardContent>
-						</CardActionArea>
+						<CardHeader
+							title={project.name}
+							subheader={new Date(project.created_at).toLocaleString()}
+							action={
+								<IconButton onClick={openMenu(index)}>
+									<MoreVertIcon />
+								</IconButton>
+							}
+						/>
+						<Menu open={index === openMenuIndex} onClose={closeMenu} anchorEl={anchor}>
+							<MenuItem onClick={closeMenu}>Delete</MenuItem>
+							<MenuItem onClick={closeMenu}>View</MenuItem>
+						</Menu>
+						<CardContent>
+							<Typography variant="body2" className={styles.projectViewCutText}>
+								{project.description}
+							</Typography>
+						</CardContent>
 					</Card>
 				</Grid>
 			))}
