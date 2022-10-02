0448e41132b5e95b7ac69f34cd766246a327cd53
Shalev Raz Israel <shalev280804@gmail.com>
2022-10-02 20:13:08 +0300

    Simplifying the useUserProjects hook

--- hooks/useUserProjects.tsx
+++ hooks/useUserProjects.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { User } from '@supabase/supabase-js';
 import { useQuery, useQueryClient } from 'react-query';
 import { getUserProjects, Project } from '../utils/supabase/projects';
@@ -10,23 +10,17 @@ export function useUserProjects(user: User | null, projects: Project[]) {
 	const { isLoading, data, refetch } = useQuery(
 		['projects'],
 		async () => {
-			if (!user) return [] as Project[];
 			// No new projects, so we can use the prefetched data;
 			if (!mutated) return projects;
-			return await getUserProjects(user.id);
+			return await getUserProjects(user!.id);
 		},
 		{
 			refetchOnWindowFocus: false,
 			initialData: projects,
 			placeholderData: projects,
+			enabled: user !== null,
 		}
 	);
-
-	useEffect(() => {
-		if (user && !isLoading) {
-			refetch();
-		}
-	}, [user, isLoading]);
 
 	function manualUpdate(newProjects: Project[]) {
 		setMutated(true);
