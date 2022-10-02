97b7a1890df6416dd631bf4834800eb933ab74c3
Shalev Raz Israel <shalev280804@gmail.com>
2022-10-02 17:06:53 +0300

    Fixed a bug where setCookie was called too often

--- hooks/useUser.tsx
+++ hooks/useUser.tsx
@@ -4,17 +4,22 @@ import { useEffect } from 'react';
 import { AuthChangeEvent, Session, User } from '@supabase/supabase-js';
 import { useQuery } from 'react-query';
 
+let prevEvent: AuthChangeEvent | null = null;
 export function useUser(): { user: User | null; isLoading: boolean } {
 	const { data: user, refetch, isLoading } = useQuery('user', getUser);
 
 	useEffect(() => {
 		const listener = supabase.auth.onAuthStateChange((event: AuthChangeEvent, session: Session | null) => {
-			fetch('/api/setCookie', {
-				method: 'POST',
-				headers: new Headers({ 'Content-Type': 'application/json' }),
-				credentials: 'same-origin',
-				body: JSON.stringify({ event, session }),
-			}).then(() => refetch());
+			console.log({ event, prevEvent });
+			if (event !== prevEvent) {
+				fetch('/api/setCookie', {
+					method: 'POST',
+					headers: new Headers({ 'Content-Type': 'application/json' }),
+					credentials: 'same-origin',
+					body: JSON.stringify({ event, session }),
+				}).then(() => refetch());
+			}
+			prevEvent = event;
 		});
 		return () => {
 			listener.data?.unsubscribe();
