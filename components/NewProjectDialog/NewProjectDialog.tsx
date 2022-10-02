68ba6c7f04b99212de1a14cff1fee1eaefd74ae4
Shalev Raz Israel <shalev280804@gmail.com>
2022-10-02 15:54:36 +0300

    NewProjectDialog now works with useMutate

--- components/NewProjectDialog/NewProjectDialog.tsx
+++ components/NewProjectDialog/NewProjectDialog.tsx
@@ -5,11 +5,14 @@ import DialogTitle from '@mui/material/DialogTitle';
 import DialogActions from '@mui/material/DialogActions';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
+import LinearProgress from '@mui/material/LinearProgress';
+import Alert from '@mui/material/Alert';
 import LogoSvg from '../LogoSvg/LogoSvg';
 import { useTheme } from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
-import { useForm, Controller, SubmitHandler } from 'react-hook-form';
+import { useForm, Controller } from 'react-hook-form';
 import { Project, createProject } from '../../utils/supabase/projects';
+import { useMutation } from 'react-query';
 
 type NewProjectDialogProps = {
 	userId: string;
@@ -25,20 +28,18 @@ interface DialogForm {
 const NewProjectDialog: FC<NewProjectDialogProps> = ({ userId, open, handleClose }) => {
 	const theme = useTheme();
 	const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
-	const { control, handleSubmit } = useForm<DialogForm>();
+	const { control, handleSubmit, reset } = useForm<DialogForm>();
 
-	const onSubmit: SubmitHandler<DialogForm> = (data) => {
-		console.log(data);
-		createProject(userId, data.name, data.description).then(
-			(proj) => {
-				console.log(proj);
-				handleClose(proj);
-			},
-			(error) => {
-				console.log(error);
-			}
-		);
-	};
+	const { isLoading, isError, mutateAsync } = useMutation(async (data: DialogForm) => {
+		const proj = await createProject(userId, data.name, data.description);
+		handleClose(proj);
+		reset();
+		return proj;
+	});
+
+	function onSubmit(data: DialogForm) {
+		mutateAsync(data);
+	}
 
 	return (
 		<Dialog open={open} onClose={() => handleClose()} maxWidth="md" fullWidth fullScreen={isSmallScreen} disableEscapeKeyDown>
@@ -46,6 +47,7 @@ const NewProjectDialog: FC<NewProjectDialogProps> = ({ userId, open, handleClose
 				<LogoSvg />
 				New Project
 			</DialogTitle>
+			{isLoading && <LinearProgress color="secondary" />}
 			<DialogContent>
 				<Controller
 					name="name"
@@ -55,7 +57,7 @@ const NewProjectDialog: FC<NewProjectDialogProps> = ({ userId, open, handleClose
 						minLength: { message: 'Project name must be at least 4 characters long', value: 4 },
 					}}
 					render={({ field, fieldState }) => {
-						return <TextField {...field} error={Boolean(fieldState.error)} helperText={fieldState.error?.message} autoFocus margin="dense" label="Project Name" type="text" fullWidth variant="standard" />;
+						return <TextField {...field} error={Boolean(fieldState.error)} helperText={fieldState.error?.message} disabled={isLoading} autoFocus margin="dense" label="Project Name" type="text" fullWidth variant="standard" />;
 					}}
 				/>
 				<Controller
@@ -66,13 +68,19 @@ const NewProjectDialog: FC<NewProjectDialogProps> = ({ userId, open, handleClose
 						minLength: { message: 'Project description must be at least 4 characters long', value: 4 },
 					}}
 					render={({ field, fieldState }) => {
-						return <TextField {...field} error={Boolean(fieldState.error)} helperText={fieldState.error?.message} autoFocus margin="dense" label="Description" placeholder="A short description of your awesome project..." type="text" multiline rows={6} fullWidth variant="standard" />;
+						return <TextField {...field} error={Boolean(fieldState.error)} helperText={fieldState.error?.message} disabled={isLoading} margin="dense" label="Description" placeholder="A short description of your awesome project..." type="text" multiline rows={6} fullWidth variant="standard" />;
 					}}
 				/>
+
+				{isError && <Alert severity="error">An error has occured, please try again</Alert>}
 			</DialogContent>
 			<DialogActions>
-				<Button onClick={() => handleClose()}>Cancel</Button>
-				<Button onClick={handleSubmit(onSubmit)}>Create</Button>
+				<Button disabled={isLoading} onClick={() => handleClose()}>
+					Cancel
+				</Button>
+				<Button disabled={isLoading} onClick={handleSubmit(onSubmit)}>
+					Create
+				</Button>
 			</DialogActions>
 		</Dialog>
 	);
