7c8be8c432db58c74511ae20a26e748af5ca41ca
Shalev Raz Israel <shalev280804@gmail.com>
2022-10-03 23:24:37 +0300

    Improved header bar 'feel' on smaller screens

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useState, useEffect } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Container from '@mui/material/Container';
 import Toolbar from '@mui/material/Toolbar';
@@ -9,11 +9,66 @@ import Box from '@mui/material/Box';
 import LogoSvg from '../LogoSvg/LogoSvg';
 import Link from 'next/link';
 import styles from './Header.module.css';
+import MenuIcon from '@mui/icons-material/Menu';
 import { useUser } from '../../hooks/useUser';
 import { logOut } from '../../utils/supabase/auth';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { IconButton, useTheme } from '@mui/material';
+import { cn } from '../../utils/general';
+import { useRouter } from 'next/router';
 
 const Header: FC = () => {
 	const { user } = useUser();
+	const router = useRouter();
+	const theme = useTheme();
+	const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
+	const [isOpen, setOpen] = useState<boolean>(false);
+
+	const contentStyles = isSmallScreen ? { color: 'primary' } : { color: 'white' };
+
+	const contents = (
+		<>
+			<Link href="/">
+				<Button sx={contentStyles}>Home</Button>
+			</Link>
+			{user ? (
+				<>
+					<Link href="/projects">
+						<Button sx={contentStyles}>Projects</Button>
+					</Link>
+					<Button sx={contentStyles} onClick={logOut}>
+						Log Out
+					</Button>
+				</>
+			) : (
+				<Link href="/auth">
+					<Button sx={contentStyles}>Log In</Button>
+				</Link>
+			)}
+		</>
+	);
+
+	useEffect(() => {
+		const onRouteChangeHandler = () => {
+			if (isOpen) {
+				setOpen(false);
+			}
+		};
+		router.events.on('routeChangeStart', onRouteChangeHandler);
+
+		document.body.style.overflow = isSmallScreen && isOpen ? 'hidden' : 'initial';
+
+		return () => {
+			document.body.style.overflow = 'initial';
+			router.events.off('routeChangeStart', onRouteChangeHandler);
+		};
+	}, [isSmallScreen, router, isOpen, setOpen]);
+
+	// useEffect(() => {
+	// 	if (isOpen) {
+	// 		setOpen(false);
+	// 	}
+	// }, [router.asPath]);
 
 	return (
 		<AppBar position="sticky">
@@ -25,29 +80,22 @@ const Header: FC = () => {
 							Shoodo
 						</ButtonBase>
 					</Link>
-					<Box sx={{ flexGrow: 1, display: 'flex', justifyContent: 'end' }}>
-						<ButtonGroup>
-							<Link href="/">
-								<Button sx={{ color: 'white' }}>Home</Button>
-							</Link>
-							{user ? (
-								<>
-									<Link href="/projects">
-										<Button sx={{ color: 'white' }}>Projects</Button>
-									</Link>
-									<Button sx={{ color: 'white' }} onClick={logOut}>
-										Log Out
-									</Button>
-								</>
-							) : (
-								<Link href="/auth">
-									<Button sx={{ color: 'white' }}>Log In</Button>
-								</Link>
-							)}
-						</ButtonGroup>
-					</Box>
+					{isSmallScreen ? (
+						<>
+							<Box sx={{ flexGrow: 1, display: 'flex', justifyContent: 'end' }}>
+								<IconButton onClick={() => setOpen(!isOpen)}>
+									<MenuIcon htmlColor="white" />
+								</IconButton>
+							</Box>
+						</>
+					) : (
+						<Box sx={{ flexGrow: 1, display: 'flex', justifyContent: 'end' }}>
+							<ButtonGroup>{contents}</ButtonGroup>
+						</Box>
+					)}
 				</Toolbar>
 			</Container>
+			<div className={cn(styles.headerOpeningMenu, isOpen && styles.headerOpeningMenuOpen)}>{contents}</div>
 		</AppBar>
 	);
 };
