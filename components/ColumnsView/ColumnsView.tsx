3742efa17efa01c4275edfdbf8d1edf2099838cb
Shalev Raz Israel <shalev280804@gmail.com>
2022-10-06 14:23:46 +0300

    Columns can now be dragged

--- components/ColumnsView/ColumnsView.tsx
+++ components/ColumnsView/ColumnsView.tsx
@@ -1,20 +1,28 @@
-import { FC, useMemo } from 'react';
+import { FC, useState, useMemo } from 'react';
 import { Column } from '../../utils/supabase/projects';
 import MovableColumn from '../MovableColumn/MovableColumn';
 import styles from './ColumnsView.module.css';
+import { Reorder } from 'framer-motion';
 
 type ColumnsViewProps = {
 	columns: Column[];
 };
 
 const ColumnsView: FC<ColumnsViewProps> = ({ columns }) => {
-	const sortedColumns = useMemo(() => columns?.sort((a, b) => b.importance - a.importance), [columns]);
+	const [cols, setCols] = useState<Column[]>(columns);
+
+	function onColsReorder(newCols: Column[]) {
+		setCols(newCols);
+	}
+
 	return (
-		<div className={styles.columnsView}>
-			{sortedColumns?.map((column) => (
-				<MovableColumn column={column} />
+		<Reorder.Group axis="x" values={cols} onReorder={onColsReorder} className={styles.columnsView} as="div">
+			{cols?.map((column: Column) => (
+				<Reorder.Item style={{ padding: 0 }} dragTransition={{ bounceDamping: 20, bounceStiffness: 200 }} key={column.id} value={column} as="div" whileDrag={{ scaleY: 1.06, boxShadow: '0px 8px 12px 4px #14141466' }}>
+					<MovableColumn column={column} />
+				</Reorder.Item>
 			))}
-		</div>
+		</Reorder.Group>
 	);
 };
 
