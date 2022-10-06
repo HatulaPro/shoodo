39168f7c9b9fca88ed4a53150f2ae33a13112101
Shalev Raz Israel <shalev280804@gmail.com>
2022-10-06 21:51:28 +0300

    improved ColumnsView styles

--- components/ColumnsView/ColumnsView.tsx
+++ components/ColumnsView/ColumnsView.tsx
@@ -5,6 +5,7 @@ import styles from './ColumnsView.module.css';
 import { Reorder } from 'framer-motion';
 import { UseMutateFunction } from 'react-query';
 import { ColumnMutateArgs } from '../../pages/projects/[id]';
+import { cn } from '../../utils/general';
 
 type ColumnsViewProps = {
 	columns: Column[];
@@ -40,7 +41,7 @@ const ColumnsView: FC<ColumnsViewProps> = ({ setColumns, columns, mutate }) => {
 	}
 
 	return (
-		<Reorder.Group axis="x" values={sortedCols} onReorder={onColsReorder} className={styles.columnsView} as="div">
+		<Reorder.Group axis="x" values={sortedCols} onReorder={onColsReorder} className={cn(styles.columnsView, 'scrollbar')} as="div" layoutScroll style={{ overflowX: 'scroll' }}>
 			{sortedCols?.map((column: Column) => (
 				<Reorder.Item style={{ padding: 0 }} dragTransition={{ bounceDamping: 20, bounceStiffness: 200 }} key={column.id} value={column} as="div" whileDrag={{ scaleY: 1.06, boxShadow: '0px 8px 12px 4px #14141466' }}>
 					<MovableColumn column={column} mutate={mutate} />
