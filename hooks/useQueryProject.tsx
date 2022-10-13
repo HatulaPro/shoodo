1a01a4ddb497a59f0ff86412e25f2de3911d50cb
Shalev Raz Israel <shalev280804@gmail.com>
2022-10-13 15:52:57 +0300

    Fixed sorting bug

--- hooks/useQueryProject.tsx
+++ hooks/useQueryProject.tsx
@@ -49,8 +49,9 @@ export function useQueryProject(user: User | null) {
 	const queryClient = useQueryClient();
 
 	const parsedProject = query.project ? (JSON.parse(query.project as string) as Project) : undefined;
-
-	console.log({ query });
+	if (parsedProject && parsedProject.columns) {
+		parsedProject.columns = sortByImportance(parsedProject.columns);
+	}
 
 	const projectId = typeof query.id === 'string' ? parseInt(query.id as string) : parsedProject?.id || -1;
 
