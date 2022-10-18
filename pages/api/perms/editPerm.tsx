ba4e826f4ce509db5435910d3ff3da347a0a3a54
Shalev Raz Israel <shalev280804@gmail.com>
2022-10-18 17:13:50 +0300

    editPerm now uses an rpc function to run faster

--- pages/api/perms/editPerm.tsx
+++ pages/api/perms/editPerm.tsx
@@ -1,8 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import type { PublicUser } from '../../../utils/supabase/auth';
 import { getServiceSupabase } from '../../../utils/supabase/client';
 import { Perm } from '../../../utils/supabase/perms';
-import { Project } from '../../../utils/supabase/projects';
 
 type ValidInput = {
 	email: string;
@@ -30,23 +28,15 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
 		if (email === user.email) return res.json({ error: 'Self invite is not allowed' });
 
-		// Checking if a user with that email exists and that the project belongs to the user
-		const [guest, project] = await Promise.all([supabase.from<PublicUser>('users').select('*').eq('email', email).single(), supabase.from<Project>('projects').select('*').eq('id', projectId).single()]);
-
-		if (guest.error) {
-			return res.json({ error: 'User not found, are you sure they have a Shoodo account?' });
-		}
-
-		if (project.error || project.data.user_id !== user.id) {
-			return res.json({ error: 'Project not found' });
-		}
-
-		const newPerm = await supabase
-			.from<Perm>('perms')
-			.upsert({ can_edit: canEdit === 'viewAndEdit', guest_id: guest.data.id, project_id: projectId }, { onConflict: 'guest_id,project_id' })
-			.single();
-
-		return res.json({ success: true, data: { ...newPerm.data, user: { id: guest.data.id, email: guest.data.email } } });
+		supabase
+			.rpc<Perm>('edit_perm', { email, pid: projectId, can_edit: canEdit === 'viewAndEdit', auth_id: user.id })
+			.single()
+			.then(({ data, error }) => {
+				if (error) {
+					return res.json({ error });
+				}
+				return res.json({ success: true, data: { ...data, user: { email: email, guest_id: data.guest_id } } });
+			});
 	} catch (e: any) {
 		return res.json({ error: e.message });
 	}
