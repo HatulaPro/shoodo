5ff4e4c08677b356269457a6be7131ce83a12e02
Shalev Raz Israel <shalev280804@gmail.com>
2022-10-18 11:53:11 +0300

    You can now edit perms in realtime

--- hooks/useRealtimeProject.tsx
+++ hooks/useRealtimeProject.tsx
@@ -1,21 +1,57 @@
 import { useEffect } from 'react';
 import { supabase } from '../utils/supabase/client';
+import { getPermById, Perm } from '../utils/supabase/perms';
 import type { Project } from '../utils/supabase/projects';
 
 export default function useRealtimeProject(project: Project | undefined, onUpdate: (proj: Project) => void) {
 	useEffect(() => {
 		if (!project) return;
-		const realtimeSubscription = supabase
+		const realtimeProjectSubscription = supabase
 			.from<Project>(`projects:id=eq.${project.id}`)
 			.on('*', (payload) => {
 				if (payload.eventType === 'UPDATE') {
-					onUpdate({ ...project, ...payload.new });
+					onUpdate({ ...project, ...payload.new } as Project);
+				} else {
+					throw new Error('Unhandled realtime event');
+				}
+			})
+			.subscribe();
+
+		const realtimePermsSubscription = supabase
+			.from<Perm>(`perms:project_id=eq.${project.id}`)
+			.on('*', (payload) => {
+				console.log(payload);
+				if (payload.eventType === 'UPDATE') {
+					const newPerms = project.perms!.map((oldPerm) => {
+						if (oldPerm.id === payload.new.id) {
+							return { ...oldPerm, ...payload.new } as Perm;
+						}
+						return oldPerm;
+					});
+					project.perms = newPerms;
+					onUpdate(project);
+				} else if (payload.eventType === 'DELETE') {
+					const newPerms = project.perms!.filter((oldPerm) => oldPerm.id !== payload.old.id);
+					project.perms = newPerms;
+					onUpdate(project);
+				} else if (payload.eventType === 'INSERT') {
+					const originalPerms = project.perms!;
+					const alreadyHasNewId = Boolean(originalPerms.find((p) => p.id === payload.new.id));
+					if (!alreadyHasNewId) {
+						const newPerms = [{ ...payload.new, user: { id: payload.new.guest_id, email: 'loading...' } } as Perm, ...originalPerms];
+						project.perms = newPerms;
+						onUpdate(project);
+						getPermById(payload.new.id).then((data) => {
+							project.perms = [data, ...originalPerms];
+						});
+					}
 				}
 			})
 			.subscribe();
 
 		return () => {
-			supabase.removeSubscription(realtimeSubscription);
+			supabase.removeSubscription(realtimeProjectSubscription);
+			supabase.removeSubscription(realtimePermsSubscription);
 		};
 	}, [supabase, project]);
 
--- utils/supabase/perms.tsx
+++ utils/supabase/perms.tsx
@@ -1,3 +1,5 @@
+import { supabase } from './client';
+
 export type Perm = {
 	id: number;
 	guest_id: string;
@@ -5,3 +7,14 @@ export type Perm = {
 	can_edit: boolean;
 	user: { id: string; email: string };
 };
+
+export async function getPermById(perm_id: number): Promise<Perm> {
+	const { data, error } = await supabase.from<Perm>('perms').select('*, user:users ( * )').eq('id', perm_id).single();
+
+	if (error) {
+		console.log(error);
+		throw new Error(error.message);
+	}
+
+	return data;
+}
